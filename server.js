be3f42bbedb61b355951255d49ceffd989629ba6
Sayan93 <sacharya1235@gmail.com>
2026-02-28 09:01:34 +0530

    Fixed static serving issue

--- server.js
+++ server.js
@@ -3,27 +3,23 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
-const authRoutes = require('./routes/authRoutes')
-const adminRoutes = require('./routes/adminRoutes')
-const teacherRoutes = require('./routes/teacherRoutes')
-
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+// 🔥 THIS IS IMPORTANT
 app.use(express.static('public'))
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected Successfully ✅'))
-  .catch((err) => console.log('MongoDB Error:', err))
-
-app.use('/api', authRoutes)
-app.use('/api/admin', adminRoutes)
-app.use('/api/teacher', teacherRoutes)
-
-const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/login.html')
 })
+
+// MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err))
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log('Server running on port', PORT))
